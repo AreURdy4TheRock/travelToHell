2f6a89eaa295db537df5051506550287454b3d0d
AreURdy4TheRock <dorkwingdock@gmail.com>
2021-04-06 21:49:56 +0300

    Seventh lesson

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@
     }
   }
 }());
+
+/* Burger heandler */
+
+(function() {
+  const burgerItem = document.querySelector('.burger')
+  const menu = document.querySelector('.header_nav')
+  const menuClose = document.querySelector('.header_nav_close')
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('header_nav_active')
+  })
+  menuClose.addEventListener('click', () => {
+    menu.classList.remove('header_nav_active')
+  })
+}());
